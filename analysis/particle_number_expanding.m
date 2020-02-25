3db8057f8080daf05cc6a6f4b5edd450dbddd88e
Alex Burden <alex.burden@outlook.com>
2020-02-25 00:09:00 +0000

    fixed universe expansion code

--- analysis/particle_number_expanding.m
+++ analysis/particle_number_expanding.m
@@ -1,14 +1,13 @@
 
 t0 = 1;
-tf = 8*pi;
-n = 1000;
+tf = 60*pi;
+n = 2000;
 
 K = 0;
-G = 155;
-% NARROW BAND: K=0.9 G=0.6325
-% BROAD BAND: K=0 G=28.28
+g = 5000;
+m = 2;
 
-[z,chi] = ode45(@(t,x) solvechi(t,x,K,G),linspace(t0,tf,n),[2 0]);
+[z,chi] = ode45(@(t,x) solvechi(t,x,K,g,m),linspace(t0,tf,n),[0.2 0]);
 
 f1 = figure(1);
 f1.Position = [100 100 600 600];
@@ -17,7 +16,8 @@ xlabel('$t$','Interpreter','latex','FontSize',23);
 ylabel('$\delta\chi_k$','Interpreter','latex','FontSize',23);
 
 a = z.^(2/3);
-w = sqrt((K./a).^2 + (((G.^2)./2).*(1-cos(2.*z))));
+phi = 1./(sqrt(3*pi).*m.*z);
+w = sqrt((K./a).^2 + (g.*phi.*sin(m.*z)).^2);
 np = (w./2).*(((abs(chi(:,2)).^2)./(w.^2)) + (abs(chi(:,1)).^2)) - 0.5;
 
 f2 = figure(2);
@@ -26,14 +26,17 @@ plot(z,log(np),'LineWidth',2);
 xlabel('$t$','Interpreter','latex','FontSize',23);
 ylabel('$\log(n_k)$','Interpreter','latex','FontSize',23);
 
-
 %legend('Broad-Band','Narrow-Band');
 
-%saveas(f2,'comparison.png')
+saveas(f1,'stochastic_long_b.png')
+saveas(f2,'stochastic_long.png')
 
-function chi = solvechi(t,x,K,G)
+function chi = solvechi(t,x,K,g,m)
 
 a = t.^(2/3);
-chi = [x(2) ; -((K./a).^2 + (((G.^2)./2).*(1-cos(2.*t)))).*x(1)];
+phi = 1./(sqrt(3*pi).*m.*t);
+w2 = (K./a).^2 + (g.*phi.*sin(m.*t)).^2;
+
+chi = [x(2) ; -w2.*x(1)];
 
 end
